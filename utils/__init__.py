a123d4de46fa67a96b016a3ad7228fbdc2d46254
Federico Silvestri <silves.federico@gmail.com>
2022-11-02 17:44:40 +0000

    Update

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,9 +1,11 @@
 """Utils package"""
-from .dataset import fetch_dataset, fetch_dataset_path
+from .dataset import fetch_dataset, fetch_dataset_path, store_preprocessed_dataset, fetch_preprocessed_dataset
 from .plot import build_grid_plot
 
 __all__ = [
     "fetch_dataset",
     "fetch_dataset_path",
+    "store_preprocessed_dataset",
+    "fetch_preprocessed_dataset",
     "build_grid_plot",
 ]
