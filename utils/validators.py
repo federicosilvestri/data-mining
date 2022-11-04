f227f0cf613cc0ad9776e15a0351ca9973590488
Federico Silvestri <silves.federico@gmail.com>
2022-11-04 11:33:49 +0000

    final 1.1 notebook part with refactoring

--- utils/validators.py
+++ utils/validators.py
@@ -4,17 +4,20 @@ Validator is a function that takes as input a value and returns True if and only
 in the domain of values.
 """
 import pandas as pd
+import re
 
 MIN_DATE = pd.Timestamp('2006-03-21')  # the date when Twitter has started the activity.
 MAX_DATE = pd.Timestamp('2022-09-28')  # the date when dataset has been collected.
 
-
 def check_int(value):
-    if pd.isna(value):
-        return False
-
     try:
-        return float(str(value)).is_integer()
+        if pd.isnull(value):
+            return False
+
+        string_value = str(value)
+        if re.match(r"^[0-9]*(\.0+)?$", string_value) is None:
+            return False
+        return float(string_value).is_integer()
     except ValueError:
         return False
 
